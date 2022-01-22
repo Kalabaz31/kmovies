2f7c12a9b497ef92549f1ce3cfeba0ff7c489b13
kalabaz31 <zahzouhy@gmail.com>
2022-01-22 11:28:00 +0100

    Master

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,40 +9,82 @@ import axios from "../helpers/axios";
 import { requests } from "../helpers/requests";
 
 const Feed = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [topRated, setTopRated] = useState([]);
   const [trendingThisWeek, setTrendingThisWeek] = useState([]);
   const [trendingToday, setTrendingToday] = useState([]);
-  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
 
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
   // FETCH POPULAR MOVIES
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.popular);
-      setPopularMovies(request.data.results);
+      setPopular(request.data.results);
       return request;
     }
     fetchData();
   }, [requests.popular]);
 
+  // FETCH Now Playinig MOVIES
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.nowPlaying);
+      setNowPlaying(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [requests.nowPlaying]);
+
+  // FETCH Now Playinig MOVIES
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.topRated);
+      setTopRated(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [requests.topRated]);
+
+  useEffect(() => {
+    setPopularMovies([
+      {
+        index: 0,
+        name: "Popular",
+        movies: popular,
+      },
+      {
+        index: 1,
+        name: "Now Playing",
+        movies: nowPlaying,
+      },
+      {
+        index: 2,
+        name: "Top Rated",
+        movies: topRated,
+      },
+    ]);
+  }, [popular, nowPlaying, topRated]);
+
   // FETCH Trending Today MOVIES
   useEffect(() => {
-    async function fetchLatest() {
+    async function fetchData() {
       const request = await axios.get(requests.trendingToday);
       setTrendingToday(request.data.results);
       return request;
     }
-    fetchLatest();
+    fetchData();
   }, [requests.topRated]);
 
   // FETCH Trending This Week MOVIES
   useEffect(() => {
-    async function fetchLatest() {
+    async function fetchData() {
       const request = await axios.get(requests.trendingThisWeek);
       setTrendingThisWeek(request.data.results);
       return request;
     }
-    fetchLatest();
+    fetchData();
   }, [requests.topRated]);
 
   useEffect(() => {
@@ -67,10 +109,7 @@ const Feed = () => {
           <Link to="/" className="flex items-center gap-1 pb-6 ">
             TV Series
           </Link>
-          <Link
-            to="/"
-            className="flex text-white items-center gap-1 pb-6"
-          >
+          <Link to="/" className="flex text-white items-center gap-1 pb-6">
             Movies
           </Link>
           <Link to="/" className="flex items-center gap-1 pb-6">
@@ -79,7 +118,11 @@ const Feed = () => {
         </div>
 
         <div className="popular">
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-row">
+              <Tabs headerTitle="" data={popularMovies} />
+            </div>
+
             <div className="flex flex-row">
               <Tabs headerTitle="Trending" data={trendingMovies} />
             </div>
--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -2,6 +2,7 @@ const API_KEY = "f62888504de69414d884fba13ee25852";
 
 export const requests = {
   popular: `/movie/popular?api_key=${API_KEY}`,
+  nowPlaying: `/movie/now_playing?api_key=${API_KEY}`,
   topRated: `/movie/top_rated?api_key=${API_KEY}`,
   trendingToday: `/trending/all/day?api_key=${API_KEY}`,
   trendingThisWeek: `/trending/all/week?api_key=${API_KEY}`,
